b46a38df656f01cd1ff75bfdf921ca53da1d1317
turnerg <turnerg@iu.edu>
2018-03-27 17:46:50 -0400

    added loop to hpcc-run.sh

--- hpcc-1.5.0/z-geo-notes/hpcc-run.sh
+++ hpcc-1.5.0/z-geo-notes/hpcc-run.sh
@@ -1,6 +1,7 @@
 #/bin/bash 
 
-mpirun -n 24 -ppn 24 ./hpcc
-./parse-hpccoutf.sh  >> hpcc-run.log
-mv hpccoutf.txt ./old/hpccoutf.`date "+%Y%m%d%H%M%S"`
-
+while (/bin/true) ; do
+  mpirun -n 24 -ppn 24 ./hpcc
+  ./parse-hpccoutf.sh  >> hpcc-run.log
+  mv hpccoutf.txt ./old/hpccoutf.`date "+%Y%m%d%H%M%S"`
+done
